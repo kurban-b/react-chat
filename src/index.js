2464625fa103a1c66f8d0f81af6796a01d7aaca8
Kurban <bdobik@mail.ru>
2021-05-05 00:57:18 +0300

    added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/configureStore';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
