ee3bd7f815cf126ba16f30646b362a41e463cbde
SheDeMere <apti2500@mail.ru>
2021-05-05 16:04:47 +0300

    received data from server

--- src/redux/ducks/contacts.js
+++ src/redux/ducks/contacts.js
@@ -10,3 +10,20 @@ export default function contacts(state = initialState, action) {
 // тут экшн креэйторы
 
 // тут санки
+
+export const loadContacts = () => {
+  return (dispatch) => {
+    dispatch({
+      type: 'contacts/load/start',
+    });
+
+    fetch('https://api.intocode.ru:8001/api/contacts')
+      .then((response) => response.json())
+      .then((json) => {
+        dispatch({
+          type: 'contacts/load.success',
+          payload: json,
+        });
+      });
+  };
+};
