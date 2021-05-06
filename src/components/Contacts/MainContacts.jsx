64e8d7dc7850e3a1dc491ef38d21327c1afa0fd3
SheDeMere <apti2500@mail.ru>
2021-05-06 17:32:29 +0300

    added active background

--- src/components/Contacts/MainContacts.jsx
+++ src/components/Contacts/MainContacts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import Contact from './Contact';
 import Search from './Search';
-
+import styles from './contacts.module.css'
 function MainContacts(props) {
   const filter = useSelector((state) => state.contacts.filter);
   const contacts = useSelector((state) => state.contacts.contacts);
@@ -10,7 +10,7 @@ function MainContacts(props) {
     (contact) => contact.fullname.indexOf(filter) > -1,
   );
   return (
-    <div>
+    <div className={styles.main_contact}>
       <Search />
       <ul>
         {filtered.map((contact) => {
