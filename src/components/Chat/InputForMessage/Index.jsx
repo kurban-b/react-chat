35916bbc39ef77460ec56673db717c76e3d6206f
Kurban <bdobik@mail.ru>
2021-05-06 15:15:28 +0300

    added sending messages

--- src/components/Chat/InputForMessage/Index.jsx
+++ src/components/Chat/InputForMessage/Index.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../chat.module.css';
 import ButtonAddMessage from './Button/Index';
+import { useParams } from 'react-router-dom';
 
-function InputForMessage(props) {
+function InputForMessage() {
+  const [textMessage, setTextMessage] = useState('');
+  const idContact = useParams().id;
+
+  const handleMessageInput = (e) => {
+    setTextMessage(e.target.value);
+  };
+
   return (
     <div className={styles.input__block}>
-      <input type="text" className={styles.input__message} />
-      <ButtonAddMessage />
+      <input
+        type="text"
+        className={styles.input__message}
+        placeholder="Введите сообщение"
+        value={textMessage}
+        onChange={handleMessageInput}
+      />
+      <ButtonAddMessage content={textMessage} idContact={idContact} />
     </div>
   );
 }
