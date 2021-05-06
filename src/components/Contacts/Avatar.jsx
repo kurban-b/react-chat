331042c4628eced67e0c7a928b1a0bc66c65a7c4
SheDeMere <apti2500@mail.ru>
2021-05-06 18:40:34 +0300

    added online

--- src/components/Contacts/Avatar.jsx
+++ src/components/Contacts/Avatar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './contacts.module.css'
 function Avatar (props) {
-  const name = props.conacts.fullname.substr(0,1)
+  const name = props.conacts.fullname.substr(0,1);
+  const online = props.conacts.online;
+
+
   return (
     <div className={styles.avatar}>
+      <p className={online ? styles.online : ''}></p>
       <p>{name}</p>
     </div>
   );
