fcf1d4a964b8afc308db2aed6357d129f63a741d
SheDeMere <apti2500@mail.ru>
2021-05-09 20:34:27 +0300

    fix sceleton

--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -7,9 +7,6 @@ import Avatar from './Avatar';
 import { useSelector } from 'react-redux';
 import Time from './Info/Time';
 
-
-
-
 function Contact(props) {
   const darkTheme = useSelector((state) => state.application.darkTheme);
   const id = useSelector((state) => state.messages.activeContactId);
@@ -17,30 +14,30 @@ function Contact(props) {
   //Активный чат (выделенный бэкграунд)
 
   return (
-      <div
-        className={
-          id === props.contact._id
-            ? `${styles.selected} ${styles.contacts}`
-            : styles.contacts
-        }
+    <div
+      className={
+        id === props.contact._id
+          ? `${styles.selected} ${styles.contacts}`
+          : styles.contacts
+      }
+    >
+      <NavLink
+        className={darkTheme ? styles.link_dark : ''}
+        to={`/contact/${props.contact._id}`}
       >
-        <NavLink
-          className={darkTheme ? styles.link_dark : ''}
-          to={`/contact/${props.contact._id}`}
-        >
-          <li>
-            <Avatar conacts={props.contact} />
-            <div className={styles['info_block']}>
-              <NameContacts contacts={props.contact} />
-              <LastMessages contacts={props.contact} />
-            </div>
-            <div className={styles.time}>
-              <Time contacts={props.contact} />
-            </div>
-          </li>
-        </NavLink>
-      </div>
-  )
+        <li>
+          <Avatar conacts={props.contact} />
+          <div className={styles['info_block']}>
+            <NameContacts contacts={props.contact} />
+            <LastMessages contacts={props.contact} />
+          </div>
+          <div className={styles.time}>
+            <Time contacts={props.contact} />
+          </div>
+        </li>
+      </NavLink>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Contacts/Preloader.jsx
+++ src/components/Contacts/Preloader.jsx
@@ -1,30 +1,150 @@
 import React from 'react';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
-import styles from './contacts.module.css'
-function Preloader (props) {
+import styles from './contacts.module.css';
+function Preloader(props) {
   return (
-   <div className={styles.preloader}>
-     <SkeletonTheme color='lightgray'>
-       <div className={styles.preloader_div}>
-         <Skeleton
-           duration={2}
-           width={50}
-           height={50}
-           style={{display: 'block'}}
-           className={styles.preloader_circle}
-           circle={true}
-           count={8}
-         />
-         <Skeleton
-           count={8}
-           duration={2}
-           height={23}
-           style={{display: 'block'}}
-           className={styles.preloader_name}
-         />
-       </div>
-     </SkeletonTheme>
-   </div>
+    <div className={styles.preloader}>
+      <SkeletonTheme color="lightgray">
+        <div className={styles.preloader_div}>
+          <Skeleton
+            duration={2}
+            width={50}
+            height={50}
+            style={{ display: 'block' }}
+            className={styles.preloader_circle}
+            circle={true}
+            count={1}
+          />
+          <Skeleton
+            count={2}
+            duration={2}
+            height={10}
+            style={{ display: 'block' }}
+            className={styles.preloader_name}
+          />
+        </div>
+      </SkeletonTheme>
+      <SkeletonTheme color="lightgray">
+        <div className={styles.preloader_div}>
+          <Skeleton
+            duration={2}
+            width={50}
+            height={50}
+            style={{ display: 'block' }}
+            className={styles.preloader_circle}
+            circle={true}
+            count={1}
+          />
+          <Skeleton
+            count={2}
+            duration={2}
+            height={10}
+            style={{ display: 'block' }}
+            className={styles.preloader_name}
+          />
+        </div>
+      </SkeletonTheme>
+      <SkeletonTheme color="lightgray">
+        <div className={styles.preloader_div}>
+          <Skeleton
+            duration={2}
+            width={50}
+            height={50}
+            style={{ display: 'block' }}
+            className={styles.preloader_circle}
+            circle={true}
+            count={1}
+          />
+          <Skeleton
+            count={2}
+            duration={2}
+            height={10}
+            style={{ display: 'block' }}
+            className={styles.preloader_name}
+          />
+        </div>
+      </SkeletonTheme>
+      <SkeletonTheme color="lightgray">
+        <div className={styles.preloader_div}>
+          <Skeleton
+            duration={2}
+            width={50}
+            height={50}
+            style={{ display: 'block' }}
+            className={styles.preloader_circle}
+            circle={true}
+            count={1}
+          />
+          <Skeleton
+            count={2}
+            duration={2}
+            height={10}
+            style={{ display: 'block' }}
+            className={styles.preloader_name}
+          />
+        </div>
+      </SkeletonTheme>
+      <SkeletonTheme color="lightgray">
+        <div className={styles.preloader_div}>
+          <Skeleton
+            duration={2}
+            width={50}
+            height={50}
+            style={{ display: 'block' }}
+            className={styles.preloader_circle}
+            circle={true}
+            count={1}
+          />
+          <Skeleton
+            count={2}
+            duration={2}
+            height={10}
+            style={{ display: 'block' }}
+            className={styles.preloader_name}
+          />
+        </div>
+      </SkeletonTheme>
+      <SkeletonTheme color="lightgray">
+        <div className={styles.preloader_div}>
+          <Skeleton
+            duration={2}
+            width={50}
+            height={50}
+            style={{ display: 'block' }}
+            className={styles.preloader_circle}
+            circle={true}
+            count={1}
+          />
+          <Skeleton
+            count={2}
+            duration={2}
+            height={10}
+            style={{ display: 'block' }}
+            className={styles.preloader_name}
+          />
+        </div>
+      </SkeletonTheme>
+      <SkeletonTheme color="lightgray">
+        <div className={styles.preloader_div}>
+          <Skeleton
+            duration={2}
+            width={50}
+            height={50}
+            style={{ display: 'block' }}
+            className={styles.preloader_circle}
+            circle={true}
+            count={1}
+          />
+          <Skeleton
+            count={2}
+            duration={2}
+            height={10}
+            style={{ display: 'block' }}
+            className={styles.preloader_name}
+          />
+        </div>
+      </SkeletonTheme>
+    </div>
   );
 }
 
