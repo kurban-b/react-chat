f61255485616a2c5f8b889e5051bc54f4e96f410
aslan9569 <1234aslan1234@mail.ru>
2021-05-11 12:38:27 +0300

    new commit 7

--- src/components/Profile/Social/Social.jsx
+++ src/components/Profile/Social/Social.jsx
@@ -7,7 +7,7 @@ function Social(props) {
   return (
     <div>
       {props.filteredContacts.map((contact) => {
-        return <UserName contact={contact} key={contact._id} />;
+        return <UserName contact={contact} key={contact._id} darkTheme={props.darkTheme} />;
       })}
     </div>
   );
